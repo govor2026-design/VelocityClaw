d628542e544950232e82ab4a12901b87a9d7e8fb
govor2026-design <govor2026@gmail.com>
2026-06-22 17:22:27 +0700

    Install run profile schema for all MemoryStore imports

--- velocity_claw/memory/__init__.py
+++ velocity_claw/memory/__init__.py
@@ -1,1 +1,8 @@
 """Velocity Claw memory package."""
+
+from velocity_claw.memory.run_profile_schema import install_run_profile_schema
+from velocity_claw.memory.store import MemoryStore
+
+install_run_profile_schema(MemoryStore)
+
+__all__ = ["MemoryStore"]
