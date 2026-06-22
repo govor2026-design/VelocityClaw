8cc3ce710b72c9fb4d5184d913122b58b0595a64
govor2026-design <govor2026@gmail.com>
2026-06-22 14:37:55 +0700

    Enable repo-aware memory v2

--- velocity_claw/memory/__init__.py
+++ velocity_claw/memory/__init__.py
@@ -1,1 +1,8 @@
 """Velocity Claw memory package."""
+
+from velocity_claw.memory.repo_context import install_repo_aware_memory_v2
+from velocity_claw.memory.store import MemoryStore
+
+install_repo_aware_memory_v2(MemoryStore)
+
+__all__ = ["MemoryStore"]
