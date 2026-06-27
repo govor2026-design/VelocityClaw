264d21d8314a0e643c43708e9aaa3cd3aabfb8ee
govor2026-design <govor2026@gmail.com>
2026-06-27 19:40:20 +0700

    chore: annotate Telegram handler registration (#172)
    * chore: annotate handler registration
    
    * test: cover handler registration annotation

--- tests/test_telegram_bot_annotations.py
+++ tests/test_telegram_bot_annotations.py
@@ -7,3 +7,9 @@ def test_telegram_bot_initializer_declares_none_return() -> None:
     hints = get_type_hints(VelocityClawTelegramBot.__init__)
 
     assert hints["return"] is type(None)
+
+
+def test_handler_registration_declares_none_return() -> None:
+    hints = get_type_hints(VelocityClawTelegramBot._register_handlers)
+
+    assert hints["return"] is type(None)
